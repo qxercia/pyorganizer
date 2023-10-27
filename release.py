ec4d9715cf5b6d7eff906f4c140113898365937d
oakvx <96045730+qxercia@users.noreply.github.com>
2023-10-27 17:53:43 +0200

    fix release.py

--- release.py
+++ release.py
@@ -1,22 +1,19 @@
 import os
 import shutil
 
-source_folder = 'D:'
+source_folder = input("Enter the source folder path: ")
 
-destination_folder = 'D:'
+destination_folder = input("Enter the destination folder path: ")
 
 for filename in os.listdir(source_folder):
     source_file = os.path.join(source_folder, filename)
 
     if os.path.isfile(source_file):
-        # Estrai il nome del file (senza estensione)
         file_name, file_extension = os.path.splitext(filename)
         
-        # Crea una sottocartella con il nome del file (se non esiste già)
         target_folder = os.path.join(destination_folder, file_name)
         os.makedirs(target_folder, exist_ok=True)
         
-        # Sposta il file nella sottocartella
         shutil.move(source_file, os.path.join(target_folder, filename))
 
-print('Operazione completata.')
+print('Operation completed.')
